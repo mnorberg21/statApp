af12622d7617a71d4de475ea28732a775f984b9d
Matthew Norberg <mnorberg@umass.edu>
2022-08-14 07:39:07 -0400

    Semi-persistent storage enabled

--- js/table.js
+++ js/table.js
@@ -1,22 +1,39 @@
 
-const NUM_ROWS = 6;
-
-const API_BASE = "http://172.17.0.3:5001/helloworld";
+const NUM_COLS = 6;
 
 window.onload = () => {
-  fetch('http://127.0.0.1:5001/helloworld').then(res => {
-    return res.json();
-  }).then(text => {
-    console.log('GET response:');
-    console.log(text);
-    console.log(text.greeting);
-  });
 
   const formRow = document.getElementById('formRow');
   formRow.style.display = "none";
 
   const saveButton = document.getElementById('save');
   saveButton.style.display = "none";
+
+  const tableRef = document.getElementById('data');
+
+  const formMapping = {
+    0: 'date',
+    1: 'course',
+    2: 'score',
+    3: 'fairways',
+    4: 'greens',
+    5: 'threePutts'
+  };
+
+  // Initialize table
+  fetch('http://127.0.0.1:5001/rest').then(res => {
+    return res.json();
+  }).then(text => {
+    console.log('GET Initialization Response:');
+    console.log(text);
+    for (let i = 0; i < text.length; i++) {
+      let newRow = tableRef.insertRow(tableRef.rows.length - 1);
+      for (let j = 0; j < NUM_COLS; j++) {     
+        let newCell = newRow.insertCell(j);
+        newCell.innerHTML = (text[i])[formMapping[j]];
+      }
+    }
+  });
 
   const addBtn = document.getElementById('addScore');
   addBtn.addEventListener('click', () => {
@@ -31,28 +48,37 @@ window.onload = () => {
     }
   });
 
-  saveButton.addEventListener('click', () => {
-    // Get a reference to the table
-    const tableRef = document.getElementById('data');
-        
+  saveButton.addEventListener('click', () => {        
     // Insert a row at the end of the table
     let newRow = tableRef.insertRow(tableRef.rows.length - 1);
 
-    const formMapping = {
-      0: 'date',
-      1: 'course',
-      2: 'score',
-      3: 'fairways',
-      4: 'greens',
-      5: 'threePutts'
-    };
+    // Store the new object to be submitted
+    let addObj = {}
 
-    // Insert a row cells
-    for (let i = 0; i < NUM_ROWS; i++) {     
+    // Insert row cells
+    for (let i = 0; i < NUM_COLS; i++) {     
       let newCell = newRow.insertCell(i);
       newCell.innerHTML = document.getElementById(formMapping[i]).value;
+      addObj[formMapping[i]] = document.getElementById(formMapping[i]).value;
       document.getElementById(formMapping[i]).value = null;
     }
+
+    // Post request options
+    let options = {
+      method: 'POST',
+      body: JSON.stringify(addObj),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    };
+
+    // Post operation status
+    fetch('http://127.0.0.1:5001/rest', options).then(res => {
+      return res.json();
+    }).then(text => {
+      console.log('POST Operation Status');
+      console.log(text);
+    });
 
     saveButton.style.display = 'none';
     formRow.style.display = 'none';
--- server/serve.py
+++ server/serve.py
@@ -1,16 +1,49 @@
+import pandas as pd
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+df = pd.DataFrame({
+    'date': ['8/5/2022', '8/6/2022'],
+    'course': ['Winchester Country Club', 'Eastward Ho!'],
+    'score': ['75', '77'],
+    'fairways': ['11', '15'],
+    'greens': ['14', '12'],
+    'threePutts': ['0', '1'],
+})
+
 class HelloWorld(Resource):
     def get(self):
         return {'greeting': 'Hello World'}
 
+class Stats(Resource):
+    def get(self):
+        return {'total': df.shape[0]}
+
+rest_parser = reqparse.RequestParser()
+rest_parser.add_argument('date', type=str)
+rest_parser.add_argument('course', type=str)
+rest_parser.add_argument('score', type=str)
+rest_parser.add_argument('fairways', type=str)
+rest_parser.add_argument('greens', type=str)
+rest_parser.add_argument('threePutts', type=str)
+
+class Rest(Resource):
+    def get(self):
+        return df.to_dict(orient = 'records')
+    
+    def post(self):
+        args = rest_parser.parse_args()
+        df.loc[len(df.index)] = args
+        return {'status': 'data item posted'}
+
 api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(Stats, "/stats")
+api.add_resource(Rest, "/rest")
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5001, host = "0.0.0.0")
